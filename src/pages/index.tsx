d2ad749dbf9a57a94e8b7c92b299d5aed58e129a
TacosdeAsada123 <marlon_021012@hotmail.com>
2023-12-11 15:16:03 -0600

    confirmado

---
+++ src/pages/index.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect, use } from "react";
+import Link from "next/link";
+import styles from "../styles/Home.module.css"
+
+
+export default function Home(){
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonName, setPokemonName] = useState([]);
+  const [pokemonId, setPokemonId] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+    .then(res => res.json())
+    .then ((data) => {
+      setPokemon(data.results);
+      setPokemonName(data.results.map((pokemon: any) => pokemon.name));
+        setPokemonId(data.results.map((pokemon: any) => pokemon.url.split('/')[6]));
+        console.log(data.results);
+    })
+  }, [])
+
+
+  return (
+    <main>
+      <h1 className={styles.page_title}>Pokemon - First Generation</h1>
+      <ol className={styles.pokemon_list}>
+        {
+          pokemon.map((pokemon, index) => {
+            return (
+              <li key={index}>
+                <Link legacyBehavior href={`/pokemon/${pokemonId[index]}`}>
+                  <a>{pokemonName[index]}</a>
+                </Link>
+              </li>
+            )
+          })
+        }
+      </ol>
+    </main>
+  )
+}
